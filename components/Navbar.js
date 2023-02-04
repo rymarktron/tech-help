cdc15e7ec7b470a61818974907f92f06c3a7a13a
rymarktron <87295558+rymarktron@users.noreply.github.com>
2023-02-03 21:58:58 -0500

    first push

---
+++ components/Navbar.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+const Navbar = () => {
+    return ( 
+        <h1>Hi</h1>
+     );
+}
+ 
+export default Navbar;
---
+++ components/TextRender.js
@@ -0,0 +1,14 @@
+//go online and use Cohere api. Prompt: get instructions on how to do it with good analogies for a "kid"
+// ask are you a child. adult, or senior
+// ask if they are a beginner, intermediate, or experienced.
+// accessibility means understanding the audience and presenting it in a way they will understand
+
+const TextRender = () => {
+    return ( 
+        <h1>
+            Hi
+        </h1>
+     );
+}
+ 
+export default TextRender;
